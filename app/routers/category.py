43790ee01168ba16962fe74fb94814c095081a62
Aleksey525 <alex_tolchin@mail.ru>
2025-03-11 13:04:34 +0300

    Initial commit

---
+++ app/routers/category.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/categories', tags=['category'])
+
+
+@router.get('/')
+async def get_all_categories():
+    pass
+
+
+@router.post('/')
+async def create_category():
+    pass
+
+
+@router.put('/{category_id}')
+async def update_category():
+    pass
+
+
+@router.delete('/{category_id}')
+async def delete_category():
+    pass
