469fd358cc257ee0fcb6413cd4bf75d93206c623
Aleksey525 <alex_tolchin@mail.ru>
2025-03-19 17:29:13 +0300

    Add JWT auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy import select, insert
@@ -5,9 +6,11 @@ from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from passlib.context import CryptContext
 
+from app.config import SECRET_KEY, ALGORITHM
 from app.models.user import User
 from app.schemas import CreateUser
 from app.backend.db_depends import get_db
+import jwt
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -23,6 +26,76 @@ async def get_authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], us
             headers={'WWW-Authenticate': 'Bearer'}
         )
     return user
+
+
+async def create_access_token(username: str, user_id: int, is_admin: bool, is_supplier: bool, is_customer: bool,
+                              expires_delta: timedelta):
+    payload = {
+        'sub': username,
+        'id': user_id,
+        'is_admin': is_admin,
+        'is_supplier': is_supplier,
+        'is_customer': is_customer,
+        'exp': datetime.now(timezone.utc) + expires_delta
+    }
+
+    payload['exp'] = int(payload['exp'].timestamp())
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str | None = payload.get('sub')
+        user_id: int | None = payload.get('id')
+        is_admin: bool | None = payload.get('is_admin')
+        is_supplier: bool | None = payload.get('is_supplier')
+        is_customer: bool | None = payload.get('is_customer')
+        expire: int | None = payload.get('exp')
+
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate user'
+            )
+
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied"
+            )
+
+        if not isinstance(expire, int):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid token format"
+            )
+
+        current_time = datetime.now(timezone.utc).timestamp()
+
+        if expire < current_time:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired!"
+            )
+
+        return {
+            'username': username,
+            'id': user_id,
+            'is_admin': is_admin,
+            'is_supplier': is_supplier,
+            'is_customer': is_customer,
+        }
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired!"
+        )
+    except jwt.exceptions:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate user'
+        )
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
@@ -46,12 +119,14 @@ async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user:
 async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = await get_authenticate_user(db, form_data.username, form_data.password)
 
+    token = await create_access_token(user.username, user.id, user.is_admin, user.is_supplier, user.is_customer,
+                                expires_delta=timedelta(minutes=20))
     return {
-        'access_token': user.username,
+        'access_token': token,
         'token_type': 'bearer'
     }
 
 
 @router.get('/read_current_user')
-async def read_current_user(user: User = Depends(oauth2_scheme)):
+async def read_current_user(user: User = Depends(get_current_user)):
     return user
